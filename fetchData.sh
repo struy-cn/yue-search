e38bde4f3518b097d74b144c38b7cb715135a648
StruggleYang <1724555319@qq.com>
2022-11-03 12:04:33 +0800

    新合集

--- fetchData.sh
+++ fetchData.sh
@@ -1,7 +1,8 @@
 touch public/wechat-page/2018.html && curl -s "https://mp.weixin.qq.com/s/5e3HSJQfq11zTCUoNAvggg?f=json" | jq --raw-output .content_noencode >public/wechat-page/2018.html
 touch public/wechat-page/2019.html && curl -s "https://mp.weixin.qq.com/s/wrG3TwBBD3lVmUjm5KpJUw?f=json" | jq --raw-output .content_noencode >public/wechat-page/2019.html
 touch public/wechat-page/2020.html && curl -s "https://mp.weixin.qq.com/s/IGVt1tWItyWcxiXZr8uO5A?f=json" | jq --raw-output .content_noencode >public/wechat-page/2020.html
-touch public/wechat-page/2021.html && curl -s "https://mp.weixin.qq.com/s/hf4fF_DgXpDk9ENcW8-LGg?f=json" | jq --raw-output .content_noencode >public/wechat-page/2021.html
+#touch public/wechat-page/2021.html && curl -s "https://mp.weixin.qq.com/s/hf4fF_DgXpDk9ENcW8-LGg?f=json" | jq --raw-output .content_noencode >public/wechat-page/2021.html
+touch public/wechat-page/2021~2022.html && curl -s "https://mp.weixin.qq.com/s/INAneQU2gGict0ikdAED6w?f=json" | jq --raw-output .content_noencode >public/wechat-page/2021~2022.html
 
 datas=$(ls public/wechat-page/)
 echo '[' >public/db/data.json
